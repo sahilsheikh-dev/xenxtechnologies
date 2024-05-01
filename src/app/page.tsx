19bff229dbb539ab6ed3e0d517b4d1f0011536fa
Sahil Sheikh <sahilsheikh.dev@gmail.com>
2024-05-01 20:43:11 +0530

    reactJs-NextJsTypescriptMigration-01052024:2043-sahilsheikh.dev@gmail.com

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { HeroParallaxSection } from "@/components/heroSection/hero-parallaxComponent";
+
+export default function Home() {
+  return (
+    <>
+      <HeroParallaxSection></HeroParallaxSection>
+      <h1>havb fsdjklhb</h1>
+    </>
+  );
+}
--- src/components/homepage/HeroSection/HeroAnimation.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const HeroAnimation = () => {
-  return (
-    <div>HeroAnimation</div>
-  )
-}
-
-export default HeroAnimation
