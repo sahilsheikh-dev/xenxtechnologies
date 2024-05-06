c1c4a219ff29fbadd1c1800a9be2fb4fcab987e8
Sahil Sheikh <sahilsheikh.dev@gmail.com>
2024-05-06 20:15:23 +0530

    navbarbgchanges-06052024:2015-sahilsheikh.dev@gmail.com

--- src/components/common/navigationSection/navbar-menu.tsx
+++ src/components/common/navigationSection/navbar-menu.tsx
@@ -56,7 +56,7 @@ export const FloatingNav = ({
           duration: 0.2,
         }}
         className={cn(
-          "flex max-w-100 fixed top-0 inset-x-0 mx-auto bg-zinc-900 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] pr-2 pl-8 py-5  items-center justify-center space-x-4 py-5",
+          "flex max-w-100 fixed top-0 inset-x-0 mx-auto bg-zinc-950 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] pr-2 pl-8 py-5  items-center justify-center space-x-4 py-5",
           className
         )}
       >
