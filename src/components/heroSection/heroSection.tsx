1a7d5a51cfde2a8b74db1a08ef4641f22a9749f8
Sahil Sheikh <sahilsheikh.dev@gmail.com>
2024-05-06 19:24:02 +0530

    navbarUpdated-06052024:1924-sahilsheikh.dev@gmail.com

--- src/components/heroSection/heroSection.tsx
+++ src/components/heroSection/heroSection.tsx
@@ -8,7 +8,7 @@ export interface IAppProps {}
 export function HeroSection(props: IAppProps) {
   return (
     <section className="">
-      <div className="px-6 py-12 text-center md:px-12 lg:text-left">
+      <div className="px-6 py-24 text-center md:px-12 lg:text-left">
         <div className="w-100 mx-auto sm:max-w-2xl md:max-w-3xl lg:max-w-5xl xl:max-w-7xl">
           <div className="grid items-center gap-12 lg:grid-cols-1">
             <div className="lg:mb-0">
